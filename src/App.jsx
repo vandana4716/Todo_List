7cf9ceb4bbf015e34e0f81c4d948594a0076fd71
mehakchroex <mehak.chroex@gmail.com>
2024-07-30 21:03:33 +0530

    finish

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import Todo from './components/Todo'
+
+
+const App = () => {
+  return (
+    <div className='bg-stone-800 grid py-4 min-h-screen'>
+      <Todo/>
+    </div>
+  )
+}
+
+export default App
